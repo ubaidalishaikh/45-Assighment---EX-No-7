6bc5a987c827a159cffede253b2eff2d7661744b
ubaidalishaikh <161380272+ubaidalishaikh@users.noreply.github.com>
2024-03-19 01:56:18 +0500

    Add files via upload
    I've completed 7th Assighment 🎉 Governer Sindh IT Intiative Kamran Khan Tessori

---
+++ Q7.js
@@ -0,0 +1,9 @@
+"use strict";
+let number1 = 4 + 4;
+console.log(number1);
+let number2 = 16 - 8;
+console.log(number2);
+let number3 = 4 * 2;
+console.log(number3);
+let number4 = 16 / 2;
+console.log(number4);
---
+++ Q7.ts
@@ -0,0 +1,25 @@
+let number1: number = 4+4;
+console.log(number1);
+
+
+let number2: number = 16-8;
+console.log(number2);
+
+
+
+let number3: number = 4*2;
+console.log(number3);
+
+
+
+let number4: number = 16/2;
+console.log(number4);
+
+
+
+
+
+
+
+
+
